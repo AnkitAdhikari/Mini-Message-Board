3c61b5c05634d8d15fbe41943123f565fb16178e
AnkitAdhikari <ankitadhikari31512@gmail.com>
2024-08-25 18:47:21 +0545

    add feat: can create new message

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,10 +1,34 @@
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date()
+    },
+    {
+        text: "Express is fun isn't it?",
+        user: "Ankit",
+        added: new Date()
+    }
+];
+
+
 const getAllMessages = (req, res) => {
-    res.render('messages');
+    res.render('messages', { messages: messages });
 }
 
 const getNewMessageForm = (req, res) => {
-    res.render('newMessageForm');
+    res.render('form');
+}
+
+const createNewMessage = (req, res) => {
+    messages.push({ ...req.body, added: new Date() });
+    res.redirect("/");
 }
 
 
-module.exports = { getAllMessages, getNewMessageForm };
+module.exports = { getAllMessages, getNewMessageForm, createNewMessage };
--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
-const { getAllMessages, getNewMessageForm } = require('../controllers/messageController');
+const { getAllMessages, getNewMessageForm, createNewMessage } = require('../controllers/messageController');
 
 const messageRouter = Router();
 
 messageRouter.get('/', getAllMessages)
 messageRouter.get('/new', getNewMessageForm);
+messageRouter.post('/new', createNewMessage);
 
 module.exports = messageRouter;
